e1414cbdd8e247485bcaa1fa104ecf0288473431
Alex Sartan <asartan@pivotal.io>
2018-05-16 16:31:53 -0600

    Enable service discovery

--- IntegrationTest/FlowTest.cs
+++ IntegrationTest/FlowTest.cs
@@ -25,6 +25,7 @@ namespace IntegrationTest
                 .AppName("RegistrationServer")
                 .Port(8883)
                 .Database("tracker_registration_dotnet_test")
+                .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDREGISTERWITHEUREKA", "false")
                 .Build();
 
             _allocationsServer = TestAppServerBuilder()
@@ -32,6 +33,7 @@ namespace IntegrationTest
                 .Port(8881)
                 .Database("tracker_allocations_dotnet_test")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
+                .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
                 .Build();
 
             _backlogServer = TestAppServerBuilder()
@@ -39,6 +41,7 @@ namespace IntegrationTest
                 .Port(8882)
                 .Database("tracker_backlog_dotnet_test")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
+                .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
                 .Build();
 
             _timesheetsServer = TestAppServerBuilder()
@@ -46,6 +49,7 @@ namespace IntegrationTest
                 .Port(8884)
                 .Database("tracker_timesheets_dotnet_test")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
+                .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
                 .Build();
         }
 
